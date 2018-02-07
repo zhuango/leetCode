75f14014a508988d42b9e8fa3b414bcb58a54393
zhuango <zhuangliu1992@mail.dlut.edu.cn>
2018-02-08 00:21:04 +0800

    731

--- cppForLeetcode/731_MyCalendarII.cpp
+++ cppForLeetcode/731_MyCalendarII.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <list>
 #include <tuple>
 using namespace std;
@@ -15,12 +17,49 @@ public:
     }
     bool book(int start, int end) 
     {
-        
+        for (auto head : singleBook)
+        {
+            int tempStart = get<0>(head);
+            int tempEnddd = get<1>(head);
+            int singleOverlapStart = max(start, tempStart);
+            int singleOverlapEnddd = min(end, tempEnddd);
+            if ( singleOverlapStart < singleOverlapEnddd )
+            {
+                for (auto doubleHead : doubleBook)
+                {
+                    int doubleStart = get<0>(doubleHead);
+                    int doubleEnddd = get<1>(doubleHead);
+                    int doubleOverlapStart = max(singleOverlapStart, doubleStart);
+                    int doubleOverlapEnddd = min(singleOverlapEnddd, doubleEnddd);
+                    if ( doubleOverlapStart < doubleOverlapEnddd )
+                    {
+                        doubleBook.clear();
+                        return false;
+                    }
+                }
+                doubleBook.push_back(make_tuple(singleOverlapStart, singleOverlapEnddd));
+            }
+        }
+        singleBook.push_back(make_tuple(start, end));
+        return true;
     }
 };
 
-/**
- * Your MyCalendarTwo object will be instantiated and called as such:
- * MyCalendarTwo obj = new MyCalendarTwo();
- * bool param_1 = obj.book(start,end);
- */
+int main(void)
+{
+    MyCalendarTwo *obj = new MyCalendarTwo();
+    bool param_1 = obj->book(10,20); 
+    cout << param_1 << endl; 
+    param_1 = obj->book(50,60); 
+    cout << param_1 << endl; 
+    param_1 = obj->book(10,40); 
+    cout << param_1 << endl; 
+    param_1 = obj->book(5,15); 
+    cout << param_1 << endl; 
+    param_1 = obj->book(5,10); 
+    cout << param_1 << endl; 
+    param_1 = obj->book(25,55); 
+    cout << param_1 << endl; 
+    // param_1 = obj->book(5,15); 
+    // cout << param_1 << endl; 
+}
