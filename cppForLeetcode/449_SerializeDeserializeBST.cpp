8c7b592ebea17b91aef7bf91d7742a509229b8db
zhuango <zhuangliu1992@mail.dlut.edu.cn>
2018-03-09 17:40:16 +0800

    449

--- cppForLeetcode/449_SerializeDeserializeBST.cpp
+++ cppForLeetcode/449_SerializeDeserializeBST.cpp
@@ -74,7 +74,7 @@ public:
         {
             strs.push_back(str);
         }
-        int index = 0;
+        int index = 1;
         vector<TreeNode*> queue;
         queue.push_back(new TreeNode(str2int(strs[0])));
         int nodeIndex = 0;
@@ -96,6 +96,7 @@ public:
                 queue.push_back(queue[nodeIndex]->right);
             }
             index += 1;
+            nodeIndex += 1;
         }
         return queue[0];
     }
@@ -119,7 +120,7 @@ private:
 int main(void)
 {
     Codec codec;
-    TreeNode *root = codec.deserialize("1 2 3 4 null 5 ");
+    TreeNode *root = codec.deserialize("41 37 44 24 39 42 48 1 35 38 40 null 43 46 49 0 2 30 36 null null null null null null 45 47 null null null null null 4 29 32 null null null null null null 3 9 26 null 31 34 null null 7 11 25 27 null null 33 null 6 8 10 16 null null null 28 null null 5 null null null null null 15 19 null null null null 12 null 18 20 null 13 17 null null 22 null 14 null null 21 23");
     string s = codec.serialize(root);
     cout << s << endl;
 }
