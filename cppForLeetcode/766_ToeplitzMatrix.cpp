7f125a0883e757be89a27c9d91603cacd1e1533d
zhuango <zhuangliu1992@mail.dlut.edu.cn>
2018-03-28 17:19:49 +0800

    665 667 670 766

---
+++ cppForLeetcode/766_ToeplitzMatrix.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        
+        for (int i = 1; i < matrix.size(); ++i)
+        {
+            for(int j = 1; j < matrix[0].size(); ++j)
+            {
+                if (matrix[i][j] != matrix[i - 1][j - 1])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
